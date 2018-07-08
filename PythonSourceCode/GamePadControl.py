cc2921e5f9e1e05d0f168a191f460fea9e197cbb
DCHartlen <devon.hartlen@dal.ca>
2018-07-08 19:36:51 -0300

    Python gamepad interface test completed. Uses gcode style parsing
    Tested using unstaged sketch. Need to add to the arduino source code.

--- PythonSourceCode/GamePadControl.py
+++ PythonSourceCode/GamePadControl.py
@@ -6,15 +6,17 @@ from pygame import init as pgInit
 # pylint:enable=E0611
 from time import sleep  # Used for pausing only
 
-class GamePadControl():
+# TODO: Why is this not callable?
+class GamePadControl:
     """
     Class handles all joystick control. All interface functions return a 4 
     element vector of the form [rx, ry, lx, ly], where r is the right joystick,
-    l is the left joystick, and x and y are the axis.
+    l is the left joystick, and x and y are the axis. 
 
     NOTE: This is for an XBox360 controller. Not sure about others.
     """
-    indexAxes = [0,1,3,4] # indices for the values of interest. Not sure what 2 is.
+    indexAxes = [0,1,4,3] # indices for the values of interest. Not sure what 2 is.
+    axesTransform = [1,-1,1,-1] # pygame returns inverse y-axis. This fixes it
     
 
     # Initialize PyGame and find joystick/gamepad
@@ -24,8 +26,8 @@ class GamePadControl():
         self.gamePad = pgCont.Joystick(0) # Note irregular python indexing
         self.gamePad.init()  # Initialize gamepad
 
-    # Gets the value of the axis.
-    def getRawAxes(self):
+    # Gets the value of the axis, floatp[-1,1]
+    def GetRawAxes(self):
         # get the current axis value (non-blocking)
         pgEvent.get()
         # Initialize output vector
@@ -33,12 +35,14 @@ class GamePadControl():
         # Iterate through indexAxes ad retrieve values
         for i in range(len(self.indexAxes)):
             axesValues[i] = self.gamePad.get_axis(self.indexAxes[i])
-        # return the axis values
+            axesValues[i] = axesValues[i]*self.axesTransform[i]
+
+        # return the axesValues (floats)
         return axesValues
 
-    def getScaledAxes(self):
+    def GetScaledAxes(self):
         # Scales input from float[-1,1] to int[-3,3]
-        axesValues = self.getRawAxes()
+        axesValues = self.GetRawAxes()
         for i in range(len(self.indexAxes)):
             # Linear interpolation from [-1,1] to [-3.5,3.5]
             axesValues[i] = int((axesValues[i]+1)*(7)/(2)-3.5)
@@ -51,8 +55,8 @@ if __name__ == "__main__":
     gamePad = GamePadControl()
 
     for i in range(20):    # Run for only 20 loops (10 sec)
-        values = gamePad.getScaledAxes()
-        print(values)
+        values = gamePad.GetScaledAxes()
+        strValues = ' '.join(str(e) for e in values)
+        # Test printing a byte array
+        print(strValues)
         sleep(0.5)
-
-
