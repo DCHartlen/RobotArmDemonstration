d60ec18d47da5f3202cedb0b71ccf52c16f7d09a
DCHartlen <devon.hartlen@dal.ca>
2018-08-04 19:41:35 -0300

    Removal of interpolation function library as it is now redundant. No impact on functionality

--- ArduinoSourceCode/InterpolationFunctions.h
+++
@@ -1,5 +0,0 @@
-// Linear interpolation/extrapolation based on two points. Integer math. 
-int InterpolationInteger(int x1, int x2, int y1, int y2, int xIn);
-
-// Linear interpolation/extrapolation based on two points. Floating point math.
-float InterpolationFloat(float x1, float x2, float y1, float y2, float xIn);
